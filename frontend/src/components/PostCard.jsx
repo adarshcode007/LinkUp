ace4af01f4e9c8e1487fb20f30c45f68ee9d872e
adarshcode007 <adarsh19pandey@gmail.com>
2025-09-20 07:49:58 +0530

    Added Connections Page

--- frontend/src/components/PostCard.jsx
+++ frontend/src/components/PostCard.jsx
@@ -2,6 +2,7 @@ import { BadgeCheck, Heart, MessageCircle, Share2 } from "lucide-react";
 import React, { useState } from "react";
 import moment from "moment";
 import { dummyUserData } from "../assets/assets";
+import { useNavigate } from "react-router-dom";
 
 const PostCard = ({ post }) => {
   const postWithHashtags = post.content.replace(
@@ -14,10 +15,15 @@ const PostCard = ({ post }) => {
 
   const handleLike = async () => {};
 
+  const navigate = useNavigate();
+
   return (
     <div className="bg-white rounded-xl shadow p-4 space-y-4 w-full max-w-2xl">
       {/* User Info */}
-      <div className="inline-flex items-center gap-3 cursor-pointer">
+      <div
+        onClick={() => navigate(`/profile/${user._id}`)}
+        className="inline-flex items-center gap-3 cursor-pointer"
+      >
         <img
           src={post.user.profile_picture}
           className="w-10 h-10 rounded-full shadow"
@@ -26,7 +32,7 @@ const PostCard = ({ post }) => {
           <span>{post.user.full_name}</span>
           <BadgeCheck className="w-4 h-4 text-blue-500" />
         </div>
-        <div>
+        <div className="text-gray-500 text-sm">
           @{post.user.username} . {moment(post.createdAt).fromNow()}
         </div>
       </div>
