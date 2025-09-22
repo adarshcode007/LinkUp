04c09b1559fe6deecdfbbd4eaf9d6186499f5981
adarshcode007 <adarsh19pandey@gmail.com>
2025-09-22 19:24:42 +0530

    Inngest file fix 2

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/", (req, res) => res.send("Server is running"));
-app.use("/api/inggest", serve({ client: inngest, functions }));
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 const PORT = process.env.PORT || 3000;
 
