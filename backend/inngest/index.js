4b08c7c368f2c9163c6f6aa2f4a1d39ed6d1d614
adarshcode007 <adarsh19pandey@gmail.com>
2025-09-23 09:31:07 +0530

    Inngest file fix 4

--- backend/inngest/index.js
+++ backend/inngest/index.js
@@ -11,7 +11,7 @@ const syncUserCreation = inngest.createFunction(
   async ({ event }) => {
     const { id, first_name, last_name, email_addresses, image_url } =
       event.data;
-    let username = email_addresses[0].email_address.split("@")[0];
+    let username = email_addresses?.[0]?.email_address?.split("@")[0];
 
     // Check availability of username
     const user = await User.findOne({ username });
@@ -23,7 +23,7 @@ const syncUserCreation = inngest.createFunction(
     const userData = {
       _id: id,
       email: email_addresses[0].email_addresses,
-      full_name: first_name + " " + last_name,
+      full_name: [first_name, last_name].filter(Boolean).join(" "),
       profile_picture: image_url,
       username,
     };
@@ -41,8 +41,8 @@ const syncUserUpdation = inngest.createFunction(
       event.data;
 
     const updatedUserData = {
-      email: email_addresses[0].email_addresses,
-      full_name: first_name + " " + last_name,
+      email: email_addresses?.[0]?.email_address || null,
+      full_name: [first_name, last_name].filter(Boolean).join(" "),
       profile_picture: image_url,
     };
 
